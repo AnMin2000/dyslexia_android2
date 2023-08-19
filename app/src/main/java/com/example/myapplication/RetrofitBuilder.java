c6df3c00ea25e5e2a624f7d8a55667a6523eaedc
AnMin <iededg@naver.com>
2023-08-19 18:26:43 +0900

    ver.2.1 RetrofitBuilder 클래스 수정,

--- app/src/main/java/com/example/myapplication/RetrofitBuilder.java
+++ app/src/main/java/com/example/myapplication/RetrofitBuilder.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,9 +10,11 @@ public class RetrofitBuilder {
     public static API api;
 
     static {
+        Gson gson = new GsonBuilder().setLenient().create();
+
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://172.16.30.138:8080/") // 요청 보내는 API 서버 url. /로 끝나야 함
-                .addConverterFactory(GsonConverterFactory.create()) // Gson을 역직렬화
+                .baseUrl("http://172.16.0.32:8080/") // 요청 보내는 API 서버 url. /로 끝나야 함
+                .addConverterFactory(GsonConverterFactory.create(gson)) // Gson을 역직렬화
                 .build();
         api = retrofit.create(API.class);
     }
