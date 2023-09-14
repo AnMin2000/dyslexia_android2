f6fe9c43597cf3f0741a7ef9b890e5ff8c54ec76
AnMin <iededg@naver.com>
2023-09-14 23:25:21 +0900

    ver.3.5.4 앨범 완성 To do : 앨범 DB 넣기

--- app/src/main/java/com/example/myapplication/PictureActivity.java
+++ app/src/main/java/com/example/myapplication/PictureActivity.java
@@ -1,37 +1,66 @@
-//package com.example.myapplication;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import android.os.Bundle;
-//import android.util.Log;
-//
-//import retrofit2.Call;
-//import retrofit2.Callback;
-//import retrofit2.Response;
-//
-//public class PictureActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_picture);
-//
-//        Call<String> call = RetrofitBuilder.api.getPictureResponse();
-//
-//        call.enqueue(new Callback<String>() {
-//            @Override
-//            public void onResponse(Call<String> call, Response<String> response) {
-//                if (response.isSuccessful()) {
-//                    Log.d("Picture", response.body());
-//                } else {
-//                    Log.e("Picture", "Picture 업로드 실패");
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<String> call, Throwable t) {
-//                Log.e("Picture", "Picture 업로드 실패", t);
-//            }
-//        });
-//    }
-//}
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.ScrollView;
+import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.ViewGroup.LayoutParams;
+import android.graphics.drawable.GradientDrawable;
+
+public class PictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+
+        TableLayout tableLayout = findViewById(R.id.tableLayout);
+
+        // 여기서 이미지 파일 이름을 변경하여 이미지를 추가하세요.
+        String[] imageFileNames = {"a8", "a8", "a8", "a8","a8", "a8", "a8", "a8","a8", "a8", "a8", "a8"};
+
+        int columnCount = 2; // 열 수
+        int rowCount = (int) Math.ceil((double) imageFileNames.length / columnCount);
+
+        Context context = this;
+
+        for (int i = 0; i < rowCount; i++) {
+            TableRow tableRow = new TableRow(context);
+
+            for (int j = 0; j < columnCount; j++) {
+                int index = i * columnCount + j;
+                if (index < imageFileNames.length) {
+                    ImageView imageView = new ImageView(context);
+                    int imageResource = getResources().getIdentifier(imageFileNames[index], "drawable", getPackageName());
+                    imageView.setImageResource(imageResource);
+
+                    // TableRow의 높이를 동적으로 조절
+                    int tableRowHeight = getResources().getDimensionPixelSize(R.dimen.image_height); // 세로 길이 145dp
+                    TableRow.LayoutParams rowParams = new TableRow.LayoutParams(
+                            TableRow.LayoutParams.MATCH_PARENT,
+                            tableRowHeight
+                    );
+                    tableRow.setLayoutParams(rowParams);
+
+                    // ImageView의 크기를 조절
+                    int imageWidth = getResources().getDimensionPixelSize(R.dimen.image_width); // 가로 길이 194dp
+                    int imageHeight = getResources().getDimensionPixelSize(R.dimen.image_height); // 세로 길이 145dp
+                    TableRow.LayoutParams params = new TableRow.LayoutParams(
+                            imageWidth,
+                            imageHeight
+                    );
+                    imageView.setLayoutParams(params);
+
+                    tableRow.addView(imageView);
+                }
+            }
+
+            tableLayout.addView(tableRow);
+        }
+    }
+
+}
+
--- app/src/main/java/com/example/myapplication/test.java
+++ app/src/main/java/com/example/myapplication/test.java
@@ -1,66 +1,66 @@
-package com.example.myapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.speech.tts.UtteranceProgressListener;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.Locale;
-
-public class test extends AppCompatActivity {
-
-    TextView textView;
-    Button button;
-    TextToSpeech textToSpeech;
-
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
-
-        textView = findViewById(R.id.textView);
-        button = findViewById(R.id.button);
-
-        String text = "hello";
-        textView.setText(text);
-
-        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    int langResult = textToSpeech.setLanguage(Locale.ENGLISH);
-
-                    if (langResult == TextToSpeech.LANG_MISSING_DATA || langResult == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        // 언어 데이터가 없거나 지원되지 않는 경우 처리
-                        // 필요에 따라 에러 처리를 추가하세요.
-                    }
-                }
-            }
-        });
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (textToSpeech != null) {
-                    String textToRead = textView.getText().toString();
-                    textToSpeech.speak(textToRead, TextToSpeech.QUEUE_FLUSH, null, null);
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onDestroy() {
-        if (textToSpeech != null) {
-            textToSpeech.stop();
-            textToSpeech.shutdown();
-        }
-        super.onDestroy();
-    }
-}
+//package com.example.myapplication;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.annotation.SuppressLint;
+//import android.os.Bundle;
+//import android.speech.tts.TextToSpeech;
+//import android.speech.tts.UtteranceProgressListener;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//import java.util.Locale;
+//
+//public class test extends AppCompatActivity {
+//
+//    TextView textView;
+//    Button button;
+//    TextToSpeech textToSpeech;
+//
+//    @SuppressLint("MissingInflatedId")
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_test);
+//
+//        textView = findViewById(R.id.textView);
+//        button = findViewById(R.id.button);
+//
+//        String text = "hello";
+//        textView.setText(text);
+//
+//        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+//            @Override
+//            public void onInit(int status) {
+//                if (status == TextToSpeech.SUCCESS) {
+//                    int langResult = textToSpeech.setLanguage(Locale.ENGLISH);
+//
+//                    if (langResult == TextToSpeech.LANG_MISSING_DATA || langResult == TextToSpeech.LANG_NOT_SUPPORTED) {
+//                        // 언어 데이터가 없거나 지원되지 않는 경우 처리
+//                        // 필요에 따라 에러 처리를 추가하세요.
+//                    }
+//                }
+//            }
+//        });
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (textToSpeech != null) {
+//                    String textToRead = textView.getText().toString();
+//                    textToSpeech.speak(textToRead, TextToSpeech.QUEUE_FLUSH, null, null);
+//                }
+//            }
+//        });
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        if (textToSpeech != null) {
+//            textToSpeech.stop();
+//            textToSpeech.shutdown();
+//        }
+//        super.onDestroy();
+//    }
+//}
