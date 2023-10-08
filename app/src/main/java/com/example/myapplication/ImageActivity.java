7ca17d0ab5a674f64514e0c855ef760c24686cad
AnMin <iededg@naver.com>
2023-10-08 23:41:19 +0900

    ver.3.6.4 사진 한번에 -> 하나하나 낱개로 수정 err : 사진이 일렬로 내려가고 사진 로딩이 너무 느림 Lazy 고려

--- app/src/main/java/com/example/myapplication/ImageActivity.java
+++ app/src/main/java/com/example/myapplication/ImageActivity.java
@@ -10,19 +10,17 @@ import android.widget.TableRow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ImageActivity extends AppCompatActivity {
 
     TableLayout tableLayout;
+    int totalPhotos; // 총 사진 갯수
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,74 +29,91 @@ public class ImageActivity extends AppCompatActivity {
 
         tableLayout = findViewById(R.id.tableLayout3);
 
-        // AsyncTask를 사용하여 서버로부터 이미지를 다운로드하고 화면에 표시
-        new DownloadImagesTask().execute("http://172.16.49.242:8080/getPhotos");
+        // 총 사진 갯수를 가져오는 AsyncTask 실행
+        new GetPhotoCountTask().execute("http://172.16.99.178:8080/getPhotoCount");
     }
 
-    private class DownloadImagesTask extends AsyncTask<String, Void, List<Bitmap>> {
+    private class GetPhotoCountTask extends AsyncTask<String, Void, Integer> {
         @Override
-        protected List<Bitmap> doInBackground(String... urls) {
+        protected Integer doInBackground(String... urls) {
             String url = urls[0];
-            List<Bitmap> bitmapList = new ArrayList<>();
 
             try {
                 HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
                 connection.connect();
                 InputStream inputStream = connection.getInputStream();
+                byte[] responseBytes = new byte[1024];
+                int bytesRead;
                 StringBuilder response = new StringBuilder();
-                int data;
-                while ((data = inputStream.read()) != -1) {
-                    response.append((char) data);
+                while ((bytesRead = inputStream.read(responseBytes)) != -1) {
+                    response.append(new String(responseBytes, 0, bytesRead));
                 }
-                JSONArray jsonArray = new JSONArray(response.toString());
+                inputStream.close();
 
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    String base64Image = jsonArray.getString(i);
-                    byte[] decodedString = android.util.Base64.decode(base64Image, android.util.Base64.DEFAULT);
-                    Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-                    bitmapList.add(bitmap);
-                }
-            } catch (IOException | JSONException e) {
+                return Integer.parseInt(response.toString());
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            return bitmapList;
+            return null;
         }
 
         @Override
-        protected void onPostExecute(List<Bitmap> bitmapList) {
-            int numColumns = 2; // 열 수
-            int rowCount = 0; // 현재 행 수
-
-            for (Bitmap bitmap : bitmapList) {
-                if (bitmap != null) {
-                    if (rowCount % numColumns == 0) {
-                        // 새로운 행을 추가
-                        TableRow currentRow = new TableRow(ImageActivity.this);
-                        tableLayout.addView(currentRow);
-
-                        // TableRow의 크기를 설정
-                        currentRow.setLayoutParams(new TableLayout.LayoutParams(
-                                TableLayout.LayoutParams.MATCH_PARENT,
-                                500)); // TableRow의 높이를 145dp로 고정
-                    }
-
-                    // ImageView를 생성하고 추가
-                    ImageView imageView = new ImageView(ImageActivity.this);
-
-                    // ImageView의 크기를 설정
-                    imageView.setLayoutParams(new TableRow.LayoutParams(
-                            250, // TableRow의 가로를 194dp로 고정
-                            280)); // TableRow의 높이를 145dp로 고정
-                    imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-                    imageView.setAdjustViewBounds(true);
-                    imageView.setImageBitmap(bitmap);
-
-                    // 현재 행에 ImageView 추가
-                    TableRow currentRow = (TableRow) tableLayout.getChildAt(rowCount / numColumns);
-                    currentRow.addView(imageView);
-                    rowCount++;
+        protected void onPostExecute(Integer result) {
+            if (result != null) {
+                totalPhotos = result;
+                // 총 사진 갯수를 확인한 후, 해당 갯수만큼 요청을 보내는 AsyncTask 실행
+                for (int i = 0; i < totalPhotos; i++) {
+                    new DownloadImageTask().execute("http://172.16.99.178:8080/getPhoto?index=" + i);
                 }
+            }
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String url = urls[0];
+
+            try {
+                HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                byte[] imageBytes = new byte[1024];
+                int bytesRead;
+                StringBuilder response = new StringBuilder();
+                while ((bytesRead = inputStream.read(imageBytes)) != -1) {
+                    response.append(new String(imageBytes, 0, bytesRead));
+                }
+                inputStream.close();
+
+                byte[] decodedString = android.util.Base64.decode(response.toString(), android.util.Base64.DEFAULT);
+                return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null) {
+                // ImageView를 생성하고 추가
+                ImageView imageView = new ImageView(ImageActivity.this);
+                imageView.setLayoutParams(new TableRow.LayoutParams(
+                        250,
+                        280));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+                imageView.setAdjustViewBounds(true);
+                imageView.setImageBitmap(bitmap);
+
+                TableRow currentRow = new TableRow(ImageActivity.this);
+                tableLayout.addView(currentRow);
+                currentRow.setLayoutParams(new TableLayout.LayoutParams(
+                        TableLayout.LayoutParams.MATCH_PARENT,
+                        500));
+                currentRow.addView(imageView);
             }
         }
     }
